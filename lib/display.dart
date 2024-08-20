bff2564cb23707d1e5eca2bd27f51224a8cf9b32
SHEBINKUNIYIL <66588611+SHEBINKUNIYIL@users.noreply.github.com>
2024-08-20 20:07:18 +0300

    Update display.dart
    secondary display issue fixed in release

--- lib/display.dart
+++ lib/display.dart
@@ -6,10 +6,10 @@ Display displayFromJson(Map<String, dynamic> json) => Display(
 
 /// for release please check response from invokeMethod
 Display displayReleaseFromJson(Map<String, dynamic> json) => Display(
-    displayId: json['a'],
-    flag: json['b'],
-    name: json['d'],
-    rotation: json['c']);
+    displayId: json['displayId'],
+    flag: json['flags'],
+    name: json['name'],
+    rotation: json['rotation']);
 
 /// The default Display id, which is the id of the built-in primary display
 /// assuming there is one.
