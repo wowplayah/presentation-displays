a6758a0482255354e73cadd05a3352bbaf648fcd
wowplayah <wowplayah@users.noreply.github.com>
2025-10-21 15:46:26 +0700

    Update PresentationDisplay.kt

--- android/src/main/kotlin/com/namit/presentation_displays/PresentationDisplay.kt
+++ android/src/main/kotlin/com/namit/presentation_displays/PresentationDisplay.kt
@@ -9,10 +9,11 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import io.flutter.embedding.android.FlutterView
 import io.flutter.embedding.engine.FlutterEngineCache
+import android.app.Activity
 
 class PresentationDisplay(context: Context, private val tag: String, display: Display) :
     Presentation(context, display) {
-
+    private var flutterView: FlutterView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,13 +26,32 @@ class PresentationDisplay(context: Context, private val tag: String, display: Di
 
         setContentView(flContainer)
 
-        val flutterView = FlutterView(context)
-        flContainer.addView(flutterView, params)
+        val view = FlutterView(context)
+        flutterView = view
+        flContainer.addView(view, params)
         val flutterEngine = FlutterEngineCache.getInstance().get(tag)
         if (flutterEngine != null) {
-            flutterView.attachToFlutterEngine(flutterEngine)
+            view.attachToFlutterEngine(flutterEngine)
         } else {
             Log.e("PresentationDisplay", "Can't find the FlutterEngine with cache name $tag")
         }
     }
+    override fun onStop() {
+        super.onStop()
+        detachFlutter()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        detachFlutter()
+    }
+
+    private fun detachFlutter() {
+        flutterView?.let {
+            try {
+                it.detachFromFlutterEngine()
+            } catch (_: Throwable) { /* no-op */ }
+        }
+    }
+
 }
